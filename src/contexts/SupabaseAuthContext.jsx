533544e9a6fb9e6ce241ca137ec0fa2e2aa8fdab
yuriBean <elizabetheden1415@gmail.com>
2025-12-10 14:48:49 +0500

    callback

--- src/contexts/SupabaseAuthContext.jsx
+++ src/contexts/SupabaseAuthContext.jsx
@@ -162,9 +162,9 @@ export const AuthProvider = ({ children }) => {
   const signInWithGoogle = useCallback(async () => {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
-      options: {
-        redirectTo: `${window.location.origin}/callback`,
-      },
+      // options: {
+      //   redirectTo: `${window.location.origin}/callback`,
+      // },
     });
 
     if (error) {
