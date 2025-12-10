7f5d113148993042ca5aad6a08bc59db3e832295
yuriBean <elizabetheden1415@gmail.com>
2025-12-10 13:00:17 +0500

    issues

--- src/pages/CallbackPage.jsx
+++ src/pages/CallbackPage.jsx
@@ -7,21 +7,18 @@ const CallbackPage = () => {
   
     useEffect(() => {
       const handleAuthRedirect = async () => {
-        // Extract the token and other parameters from the URL fragment
-        const hash = window.location.hash; // Get everything after the #
-        const params = new URLSearchParams(hash.replace('#', '?')); // Replace # with ? to treat it like a query string
+        const hash = window.location.hash; 
+        const params = new URLSearchParams(hash.replace('#', '?'));
         const access_token = params.get('access_token');
         const refresh_token = params.get('refresh_token');
   
         if (access_token) {
-          // Set session if token exists
           const { error } = await supabase.auth.setSession({ access_token, refresh_token });
   
           if (error) {
             console.error('Error setting session:', error.message);
           } else {
             console.log('User authenticated');
-            // Redirect to the dashboard or desired page
             navigate('/dashboard');
           }
         } else {
