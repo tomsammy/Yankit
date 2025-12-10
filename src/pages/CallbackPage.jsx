f0dbbb688eaf07b79de809b59c146755d42f1ff0
yuriBean <elizabetheden1415@gmail.com>
2025-12-10 12:55:22 +0500

    callback

--- src/pages/CallbackPage.jsx
+++ src/pages/CallbackPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; 
-import { supabase } from './supabaseClient'; 
+import { supabase } from '@/lib/supabaseClient';
 
 const CallbackPage = () => {
   const navigate = useNavigate(); 
