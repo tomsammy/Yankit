51a6115015eb3a8a4a11f0413aa974f41f728f90
yuriBean <elizabetheden1415@gmail.com>
2025-12-17 19:51:39 +0500

    auth fix

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,9 +1,12 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://funprubplxgwmsswwauj.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ1bnBydWJwbHhnd21zc3d3YXVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwNTQ2NTksImV4cCI6MjA2MzYzMDY1OX0.ZbUuoqEMHQboBEvuqwdtz718PMX1bZ_NEELaQaSeixg';
-
-const supabaseClient = createClient(supabaseUrl, supabaseAnonKey);
+const supabaseClient = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_ANON_KEY, {
+    auth: {
+      detectSessionInUrl: true,
+      persistSession: true,
+      autoRefreshToken: true,
+    },
+  });
 
 export default supabaseClient;
 
